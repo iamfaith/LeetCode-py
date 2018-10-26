cf18f63192fb8685421c5c2701f6c205214d4837
xianzixiang <xianzixiang@wps.cn>
2018-10-26 20:57:52 +0800

    auto

--- 213_HouseRobber.py
+++ 213_HouseRobber.py
@@ -10,17 +10,18 @@ class Solution:
             return 0
         elif count == 1:
             return nums[0]
-        dp1 = [0 for x in range(count)]
-        dp2 = [0 for x in range(count)]
+        dp1, dp2 = [0 for x in range(count)], [0 for x in range(count)]
 
         def cal_max(begin, end, nums, dp):
+            curMax = 0
             for i in range(begin, end):
                 if i > 1:
                     dp[i] = max(dp[begin:i - 1]) + nums[i]
                 else:
                     dp[i] = nums[i]
+                curMax = max(curMax, dp[i])
             # print(dp)
-            return max(dp)
+            return curMax
 
         return max(cal_max(0, count - 1, nums[:count - 1], dp1), cal_max(0, count - 1, nums[1:], dp2))
 
